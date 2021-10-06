e6f187fbfb0f38d824a782f987a223f41959a3b6
James Wu <james_wu@appdet.com>
2021-10-06 23:33:04 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === 'development' ? '/' : '/vite-react-pwa-practice/',
   plugins: [
     react(),
     VitePWA({
